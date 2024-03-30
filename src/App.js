132739cd3477b8160b4ecf6961508280691b0854
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-03-30 01:12:43 +0100

    Updated App layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import ThemeProvider from './Components/ThemeProvider';
+import ThemeComponent from './Components/ThemeComponent';
+import { connect } from 'react-redux';
+import { Text, Box } from '@chakra-ui/react';
 
-function App() {
+function App({login_info}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Box>
+      <div className="App">
+        {login_info.length>0 && login_info.map(info=>{
+        return <Text w={'92%'} ml='auto' mr={'auto'} border={'none'} mb={-14} pt={4} textAlign={'right'} 
+        fontSize={20} fontWeight={'bold'} fontFamily={'fantasy'}>{`${info.email}`}</Text>
+        })}
+          <ThemeComponent />
+      </div>
+      </Box>
+      
+    </ThemeProvider>
+    
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { userInfo } = state
+
+  return { login_info: userInfo.loginParameters}
+}
+
+export default connect(mapStateToProps)(App);
