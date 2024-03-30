b9e0ed1fa31b28ab892781c4a1305fc96ecf6a3a
Kelechi Orunta <mac@MACs-MacBook-Pro.local>
2024-03-30 01:10:24 +0100

    Updated ThemeComponent for the design and layout of the page

--- src/Components/ThemeComponent.js
+++ src/Components/ThemeComponent.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Box, Button, Heading, Grid } from '@chakra-ui/react'
+import { Box, Button, Heading, Grid, Image, Flex, Text } from '@chakra-ui/react'
 import CarouselComponent from './CarouselComponent'
 import Header from './Header'
+import backgroundpic from '../Slides/picimg1.jpg'
+import SliceComponent from './SliceComponent'
+import ModalPage from './ModalPage'
+import { useState } from 'react'
 
 
 function ThemeComponent() {
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    const handleClose = () => {
+    setIsOpen(false);
+    }
+
   return (
-    <Box m={{base:4, md:4}} borderRadius="20px" borderWidth="2px" shadow="md" 
-    w={{ base: "94.75%", sm:"94%", md: "92.5%" }} margin={{base:"3%", sm:"3%", md:'4%'}}
-    gridRow={{base:'col-12', sm:'col-sm-12', md:'col-lg-6'}}>
+    <Box borderRadius="20px" borderWidth="2px" shadow="md" 
+    w={{ base: "94.75%", sm:"94%", md: "92.5%" }} margin={{base:"4%", sm:"4%", md:'4%'}} filter={'brightness(70%)'} bgBlendMode={'lighten'}
+    gridRow={{base:'col-12', sm:'col-sm-12', md:'col-lg-6'}}  bgColor='black' position={'relative'}>
+
+        <ModalPage isOpen={isOpen} handleClose={handleClose}/>
         <Header />
-        <Heading as='h1' size={{base:'md', sm: 'lg', md:'lg'}} marginTop={{sm:"8px", md:"8px"}} marginBottom={{sm:"4px", md:"4px"}} textShadow="2px 2px 5px red" textTransform={'uppercase'}>
+        <Image position='absolute' top='0' zIndex='-999' opacity={0.3} src={backgroundpic} alt='Backgroundpic' backgroundSize={'cover'}/>
+        {/* <Heading as='h1' size={{base:'md', sm: 'lg', md:'lg'}} marginTop={{sm:"8px", md:"8px"}} marginBottom={{sm:"4px", md:"4px"}} textShadow="2px 2px 5px red" textTransform={'uppercase'}>
             Welcome to My Chakra App
-        </Heading>
-        <Button colorScheme='blue' size={{base:'md', sm: 'lg', md:'lg'}}>Click Me!</Button>
-        <Box shadow='md' borderWidth='5px' height={{base:"400px", sm:'350px', md:'300px'}}
-        />
+        </Heading> */}
+        {/* <Button colorScheme='blue' size={{base:'md', sm: 'lg', md:'lg'}}>Click Me!</Button> */}
+        <Flex shadow='md' borderWidth='5px' height={{base:"400px", sm:'350px', md:'300px'}} justifyContent='center'
+           flexDirection={'column'} alignItems={'center'} gap={'10px'} borderRadius={'50px'} borderBottomWidth={0}>
+          <Heading fontSize={{base:'80px', sm:"80px", md:"80px", lg:"120px"}} color={'white'} size={{base:'md', sm: 'lg', md:'lg'}} marginTop={{sm:"8px", md:"8px"}} 
+          m={'auto'} marginBottom={{sm:"4px", md:"4px"}} stroke={'darkcyan'} textShadow="2px 2px 5px white" textTransform={'uppercase'}>
+            WebChaka
+          </Heading>
+          <Text style={{fontSize: '50px', color:'white'}} fontFamily={'cursive'}>Your No 1 Site</Text>
+        </Flex>
+        <SliceComponent/>
         <CarouselComponent />
         <Grid
         templateRows={{base:"repeat(1, 100px)", sm:"repeat(2, 100px)", md:"repeat(3, 100px)"}}
